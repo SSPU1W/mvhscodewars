b3fc3901ccd40f7ee441d0aee968575ecd6d37ed
SSPU1W <100019692@mvla.net>
2019-02-06 09:30:45 -0800

    Old Version

---
+++ build/precache-manifest.d0b7cdcee775e789df9be1a2db5b3f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3657bf79a65c3fd136a3",
+    "url": "/mvhscodewars/static/js/runtime~main.3657bf79.js"
+  },
+  {
+    "revision": "c16d3908adf124bc0ab7",
+    "url": "/mvhscodewars/static/js/main.c16d3908.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/mvhscodewars/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "c16d3908adf124bc0ab7",
+    "url": "/mvhscodewars/static/css/main.8f9f8876.chunk.css"
+  },
+  {
+    "revision": "1b5bc0b4daa7d309cead12e90293d19a",
+    "url": "/mvhscodewars/index.html"
+  }
+];
