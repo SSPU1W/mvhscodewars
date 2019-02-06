d9041b42a42f845a60a79588b8522f9711237424
SSPU1W <100019692@mvla.net>
2019-02-06 09:19:46 -0800

    More Fixes

--- build/precache-manifest.11fe4d080601d76e6103927ac92cb7b2.js
+++ build/precache-manifest.11fe4d080601d76e6103927ac92cb7b2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mvhscodewars/static/js/runtime~main.3657bf79.js"
   },
   {
-    "revision": "a63f6b2c88aa5a15b6e6",
-    "url": "/mvhscodewars/static/js/main.a63f6b2c.chunk.js"
+    "revision": "b8fc6f3aaa00f5e4f214",
+    "url": "/mvhscodewars/static/js/main.b8fc6f3a.chunk.js"
   },
   {
     "revision": "4a34e45d34cafb7623bd",
     "url": "/mvhscodewars/static/js/1.4a34e45d.chunk.js"
   },
   {
-    "revision": "a63f6b2c88aa5a15b6e6",
+    "revision": "b8fc6f3aaa00f5e4f214",
     "url": "/mvhscodewars/static/css/main.8f9f8876.chunk.css"
   },
   {
-    "revision": "f80253603ee921d731b81ef214fddfed",
+    "revision": "3f459e35bfaa45b2f8d41f45495c8de8",
     "url": "/mvhscodewars/index.html"
   }
 ];
